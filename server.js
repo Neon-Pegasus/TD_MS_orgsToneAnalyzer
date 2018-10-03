3a78fb6aff68126d572e81848672852c78dda351
Jhia Turner <jhimitu@gmail.com>
2018-10-03 10:26:30 -0700

    integrate sentiment analysis into server

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-const express = require('express');
-
-const app = express();
+require('dotenv').load({ silent: true });
+const app = require('./tone_analyzer/index.js');
 
 app.listen(4000, () => {
   console.log('Server now listening on port 4000');
--- tone_analyzer/index.js
+++ tone_analyzer/index.js
@@ -1,26 +1,46 @@
+const express =require('express');
 require('dotenv').config();
-import ToneAnalyzerV3 from 'watson-developer-cloud/tone-analyzer/v3';
+const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
 
-const toneAnalyzer = new ToneAnalyzerV3({
-  url: process.env.TONE_ANALYZER_URL,
+const app = express();
+
+const sentiment = new NaturalLanguageUnderstandingV1({
+  url: process.env.NATURAL_LANGUAGE_UNDERSTANDING_URL,
   version: process.env.TONE_ANALYZER_VERSION_DATE,
-  iam_apikey: process.env.TONE_ANALYZER_APIKEY,
+  iam_apikey: process.env.NATURAL_LANGUAGE_UNDERSTANDING_IAM_APIKEY,
 });
 
-const text = 'There is not any official documentation dictating that right-to-left languages should add "rtl" attribute in addition to "lang" to the "html" tag, so it would be at-least helpful to let people know about this issue with this package.';
-const toneParams = {
-  tone_input: { text },
-  content_type: 'application/json',
-};
-toneAnalyzer.tone(toneParams, (error, toneAnalysis) => {
-  if (error) {
-    console.log(error);
-  } else {
-    // TODO: save toneAnalysis to a database
-    const tones = toneAnalysis;
-    console.log(tones.document_tone.tones);
-  }
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(express.static('/client'));
+
+
+app.get('/', (req, res) => {
+  res.render('/client');
 });
 
-const _toneAnalyzer = toneAnalyzer;
-export { _toneAnalyzer as toneAnalyzer };
+app.post('/', (req, res, next) => {
+  sentiment.analyze(req.body, (error, sentimentAnalysis) => {
+    if (error) {
+      console.log(error);
+    }
+      // TODO: save toneAnalysis to a database
+    console.log(sentimentAnalysis);
+  });
+});
+
+module.exports = app;
+
+
+
+
+
+
+
+
+// const text = 'There is not any official documentation dictating that right-to-left languages should add "rtl" attribute in addition to "lang" to the "html" tag, so it would be at-least helpful to let people know about this issue with this package.';
+// const toneParams = {
+//   tone_input: { text },
+//   content_type: 'application/json',
+// };
